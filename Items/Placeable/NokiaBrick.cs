7a9336e38d069c1de1d00496421b76594dbafd0d
FFuuZZuu <47675688+FFuuZZuu@users.noreply.github.com>
2020-03-28 19:26:22 +0000

    Addded Nokia Brick + Crafting recipe
    Addded Nokia Brick + Crafting recipe

---
+++ Items/Placeable/NokiaBrick.cs
@@ -0,0 +1,46 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace UnbreakableBlockV2.Items.Placeable
+{
+    public class NokiaBrick : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nokia Brick");
+            Tooltip.SetDefault("Super tough! Not even a luminite pickaxe can break it...");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 1;
+            item.height = 1;
+            item.maxStack = 999;
+            item.rare = 13;
+            item.useStyle = 1;
+            item.useTime = 10;
+            item.useAnimation = 10;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.createTile = mod.TileType("NokiaBrickTile");
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            // Ingredients: 1 Cell Phone + 15 x Nebula, Solar, Stardust and Vortex fragments
+            recipe.AddIngredient(ItemID.CellPhone);
+            recipe.AddIngredient(ItemID.FragmentNebula, 15);
+            recipe.AddIngredient(ItemID.FragmentSolar, 15);
+            recipe.AddIngredient(ItemID.FragmentStardust, 15);
+            recipe.AddIngredient(ItemID.FragmentVortex, 15);
+            // Crafted At: Heavy Work Bench
+            recipe.AddTile(TileID.HeavyWorkBench);
+            // Gives: 15 Nokia Bricks
+            recipe.SetResult(this, 15);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/NokiaBrickTile.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace UnbreakableBlockV2.Tiles
+{
+    public class NokiaBrickTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            drop = mod.ItemType("NokiaBrick");
+
+            AddMapEntry(new Color(79, 79, 79));
+
+            minPick = 300;
+
+        }
+    }
+}
